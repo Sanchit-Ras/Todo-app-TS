87a441aa92c56e727ae7e1042bdcac00a533852b
Sanchit-Ras <sanchitz123rastogi@gmail.com>
2025-07-25 18:56:14 +0530

    Todo app using TypeScript completed

--- src/TaskType.ts
+++ src/TaskType.ts
@@ -1,3 +1,4 @@
+import { type Dispatch, type SetStateAction } from 'react'
 export type task={
     id:string;
     title:string
@@ -7,21 +8,21 @@ export type tasksData=task[];
 type BaseTodoProps = {
     tasks: tasksData;
     completed: tasksData;
-    updateTasks: React.Dispatch<React.SetStateAction<tasksData>>;
+    updateTasks: Dispatch<SetStateAction<tasksData>>;
 };
 //extended base type (DRY)
 
 export type TodoListProps = BaseTodoProps & {
-    updateCompleted: React.Dispatch<React.SetStateAction<tasksData>>;
+    updateCompleted: Dispatch<SetStateAction<tasksData>>;
 };
 
 export type CreateTodoProps = BaseTodoProps & {
-    setAdd: React.Dispatch<React.SetStateAction<boolean>>;
+    setAdd: Dispatch<SetStateAction<boolean>>;
 };
 export type formikValueType={
     input:string
 }
 export type updateStateType={
-    updateCompleted: React.Dispatch<React.SetStateAction<tasksData>>;
-    updateTasks: React.Dispatch<React.SetStateAction<tasksData>>;
+    updateCompleted: Dispatch<SetStateAction<tasksData>>;
+    updateTasks: Dispatch<SetStateAction<tasksData>>;
 };
